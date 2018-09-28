3aaa2f22c320106ca94cdf6dba444596b9490ced
sahid <sahid.rojas64@gmail.com>
2018-09-28 04:03:40 -0600

    Reparacion de interfaz + sockets
    La interfaz resulto inesperadamente modificada por un pull al crear muchos conflictos y como no se pudieron resolver hubo que recuperar el código. Tambien se asienta el avance de la incorporación de sockets dentro de la interfaz en colaboración con los commits e investigación de tomas. Esta versión requirió muchas pruebas para llegar a algo funcional y por eso abarca tantos cambios

---
+++ NET/Cliente.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NET;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.net.*;
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author reds
+ */
+public class Cliente {
+    private String ip;
+    private int puerto;
+    public  Cliente(String ip_adress,int puertico){
+
+        ip=ip_adress; 
+        puerto=puertico;
+    }
+
+    public void enviar(int pos1,int pos2,String name ,int Figura, boolean inicio){
+
+        Envio E1=new Envio();
+        E1.setXy1(pos1);
+        E1.setXy2(pos2);
+        E1.setUser(name);
+        E1.setIp(this.ip);
+        E1.setInicio(inicio);
+       E1.setForma(Figura);
+        E1.setIp(ip);
+        E1.setInicio(false);
+        try {
+              System.out.println(E1.Shipout(E1));
+        } 
+        catch (JsonProcessingException ex) {
+        Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Socket S1;
+        try {
+            S1 = new Socket(ip,puerto);
+            DataOutputStream O1=new DataOutputStream(S1.getOutputStream());       
+            O1.writeUTF(E1.Shipout(E1));
+            O1.close();
+        } catch (UnknownHostException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+    }
+    public void enviar(String name){
+        Envio E1=new Envio();
+        E1.setInicio(true);
+        E1.setIp(ip);
+        E1.setUser(name);
+         try {
+              System.out.println(E1.Shipout(E1));
+        } 
+        catch (JsonProcessingException ex) {
+        Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Socket S1;
+        try {
+            S1 = new Socket(ip,puerto);
+            DataOutputStream O1=new DataOutputStream(S1.getOutputStream());       
+            O1.writeUTF(E1.Shipout(E1));
+            O1.close();
+        } catch (UnknownHostException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        
+        
+    }
+    
+    }
+}
